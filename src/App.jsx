60f4ea23f281e42684d61f934c8b17519135fe50
aniket <50738821+enigma-24@users.noreply.github.com>
2025-01-28 20:15:25 -0500

    add names to phonebook

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
-function App() {
-	return <></>;
-}
+import { useState } from 'react';
+
+const App = () => {
+	const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+	const [newName, setNewName] = useState('');
+
+	const handleNameChange = (event) => setNewName(event.target.value);
+
+	const handleAddName = (event) => {
+		event.preventDefault();
+		setPersons(persons.concat({ name: newName }));
+		setNewName('');
+	};
+
+	return (
+		<div>
+			<h2>Phonebook</h2>
+			<form onSubmit={handleAddName}>
+				<div>
+					name: <input value={newName} onChange={handleNameChange} />
+				</div>
+				<div>
+					<button type='submit'>add</button>
+				</div>
+			</form>
+			<h2>Numbers</h2>
+			{persons.map((person) => (
+				<div key={person.name}>{person.name}</div>
+			))}
+		</div>
+	);
+};
 
 export default App;
