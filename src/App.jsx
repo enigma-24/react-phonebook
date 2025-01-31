827608e1764db20ac5106a51b608a8ae205d8cdf
aniket <50738821+enigma-24@users.noreply.github.com>
2025-01-30 20:27:36 -0500

    add feature for searching a person using their name

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react';
 
 const App = () => {
-	const [persons, setPersons] = useState([{ name: '' }]);
+	const [persons, setPersons] = useState([
+		{ name: 'Abc Xyz', phoneNumber: '123-456-7890' },
+	]);
 	const [newName, setNewName] = useState('');
 	const [phoneNumber, setPhoneNumber] = useState('');
+	const [searchText, setSearchText] = useState('');
 
 	const handleNameChange = (event) => setNewName(event.target.value);
 
@@ -21,9 +24,22 @@ const App = () => {
 		setPhoneNumber('');
 	};
 
+	const peopleMatchingSearchText = persons.filter((person) =>
+		person.name.toLowerCase().includes(searchText.toLowerCase())
+	);
+
 	return (
 		<div>
 			<h2>Phonebook</h2>
+			<div>
+				filter shown with{' '}
+				<input
+					type='text'
+					value={searchText}
+					onChange={(event) => setSearchText(event.target.value)}
+				/>
+			</div>
+			<h2>add a new</h2>
 			<form onSubmit={handleAddName}>
 				<div>
 					name: <input value={newName} onChange={handleNameChange} />
@@ -37,7 +53,8 @@ const App = () => {
 				</div>
 			</form>
 			<h2>Numbers</h2>
-			{persons.map((person) => (
+
+			{peopleMatchingSearchText.map((person) => (
 				<div key={person.name}>
 					{person.name} {person.phoneNumber}
 				</div>
